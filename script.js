d17d0a37f9c7a618f6d873b2b236f17c7b471a63
alex <alexgordonind@gmail.com>
2019-11-19 21:59:50 +0300

    secondcommit

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+let money = prompt('Ваш бюджет на месяц?', ""),
+	time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD');
 
-console.log(persone.name);
+console.log(money);
+console.log(time);
+
+
+
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: '',
+	income : {},
+	savings: false
+}
+
+let	i1 = prompt('Введите обязательную статью расходов в этом месяце'),
+	i2 = prompt('Во сколько обойдется?');
+	i3 = prompt('Введите обязательную статью расходов в этом месяце'),
+	i4 = prompt('Во сколько обойдется?');
+
+appData.expenses[i1] = i2,
+appData.expenses[i3] = i4,
+
+alert(appData.budget/30);
